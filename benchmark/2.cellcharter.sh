e11cb675e641d192f3d6aed0051e4379204af5cd
LakshaySood <ureachlakshay@gmail.com>
2025-07-16 17:09:17 -0400

    cellcharter and tissuemosaic benchmarking code

---
+++ benchmark/2.cellcharter.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+#SBATCH -t 1-12:00
+#SBATCH -c 1
+#SBATCH --mem=50G
+#SBATCH --open-mode=append
+#SBATCH --job-name=cc_uc 
+#SBATCH --exclude=compute-g-16-25[4-5]
+#SBATCH -o /n/data1/hms/dbmi/raychaudhuri/lab/lakshay/vimapaper/benchmark/_data/UC/cc_uc.out 
+#SBATCH -e /n/data1/hms/dbmi/raychaudhuri/lab/lakshay/vimapaper/benchmark/_data/UC/cc_uc.err
+
+DATASET=$1 
+python "./2.cellcharter.py" $DATASET
