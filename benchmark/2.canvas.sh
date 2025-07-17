a53f1512924ac5ba41fff9af5afa729efe457c03
yakirr <yreshef@broadinstitute.org>
2025-07-17 13:53:19 -0400

    canvas bugfixes

--- benchmark/2.canvas.sh
+++ benchmark/2.canvas.sh
@@ -2,23 +2,30 @@
 
 DATASET=$1
 
+source ~/miniconda3/etc/profile.d/conda.sh
 conda activate canvas
 
+export PYTHONPATH="${PYTHONPATH}:$(pwd)/_packages/CANVAS"
+
 # preprocessing
+echo "PREPROCESSING"
 python _packages/CANVAS/canvas/run_preprocess.py \
     --config_root _data/$DATASET/canvas/configs \
     --data_root _data/$DATASET/canvas/data
 
 # training
-python packages/CANVAS/canvas/run_training.py \
+echo "TRAINING"
+python _packages/CANVAS/canvas/run_training.py \
     --epoch 200 \
     --config_root _data/$DATASET/canvas/configs \
     --data_root _data/$DATASET/canvas/data
 
-python packages/CANVAS/canvas/run_inference.py --ckpt_num 199 \
+echo "INFERENCE"
+python _packages/CANVAS/canvas/run_inference.py --ckpt_num 199 \
     --config_root _data/$DATASET/canvas/configs \
     --data_root _data/$DATASET/canvas/data
 
-python packages/CANVAS/canvas/run_analysis.py \
+echo "ANALYSIS"
+python _packages/CANVAS/canvas/run_analysis.py \
     --config_root _data/$DATASET/canvas/configs \
     --data_root _data/$DATASET/canvas/data
