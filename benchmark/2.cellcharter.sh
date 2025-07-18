15859cbbd6cebd742118293e19b5ed2a3c83ef3e
LakshaySood <ureachlakshay@gmail.com>
2025-07-18 11:53:19 -0400

    changing batch integration penalty

--- benchmark/2.cellcharter.sh
+++ benchmark/2.cellcharter.sh
@@ -2,11 +2,11 @@
 
 #SBATCH -p gpu
 #SBATCH --gres=gpu:1
-#SBATCH -t 1-12:00
+#SBATCH -t 0-08:00
 #SBATCH -c 1
 #SBATCH --mem=50G
 #SBATCH --open-mode=append
-#SBATCH --job-name=cc_uc 
+#SBATCH --job-name=cc_uc  
 #SBATCH --exclude=compute-g-16-25[4-5]
 #SBATCH -o /n/data1/hms/dbmi/raychaudhuri/lab/lakshay/vimapaper/benchmark/_data/UC/cc_uc.out 
 #SBATCH -e /n/data1/hms/dbmi/raychaudhuri/lab/lakshay/vimapaper/benchmark/_data/UC/cc_uc.err
