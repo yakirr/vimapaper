fe29d2d8359a91610d317e11cdba2e8a20366bb2
yakirr <yreshef@broadinstitute.org>
2025-07-28 22:36:23 -0400

    finished reordering main fig parts, finished RA supp figs

--- common.py
+++ common.py
@@ -33,23 +33,73 @@ def write_perplexities(dsetname, ds, outpath):
     perplexities.to_csv(outpath, index=False)
     print(perplexities.groupby('method').describe())
 
-def collate_cc(dsetname, D, outpath):
-    ps = []
-    nposs = []
-    nnegs = []
-    ns = []
-    methods = []
-    for file in glob.glob(f'../benchmark/_results/{dsetname}_*_noharm.h5ad'):
-        d = sc.read_h5ad(file)
-        methods.append(os.path.basename(file).split('_')[1])
-        ps.append(d.uns['clustercc_globalp'])
-        nposs.append(d.uns['clustercc_npos'])
-        nnegs.append(d.uns['clustercc_nneg'])
-        ns.append(len(d))
-    df = pd.DataFrame({'method':methods, 'p':ps, 'npos':nposs, 'nneg':nnegs, 'ntotal':ns}).set_index('method', drop=True)
-    df.loc['vima'] = {'p':D.uns['vima_p'], 'npos':(D.obs.sig_mncoef > 0).sum(), 'nneg':(D.obs.sig_mncoef < 0).sum(), 'ntotal':len(D)}
-    df['frac_pos'] = df.npos / df.ntotal
-    df['frac_neg'] = df.nneg / df.ntotal
-    df.sort_values('p', inplace=True)
-    df.to_csv(outpath)
-    print(df)
+def collate_cc(dsetname, D=None, outpath=None):
+    results = []
+    for f in glob.glob(f'../benchmark/_results/{dsetname}*_noharm.h5ad'):
+        method = os.path.basename(f).split('_')[1]
+        d = sc.read_h5ad(f)
+        results.append({'method': method,
+                        'microniche':False,
+                        'harmonized':False,
+                        'bonferroni':False,
+                        'p': d.uns['clustercc_globalp'],
+                        'npos': d.uns['clustercc_npos'],
+                        'nneg': d.uns['clustercc_nneg'],
+                        'ntotal': len(d)
+                        })
+        results.append({'method': method,
+                        'microniche':True,
+                        'harmonized':False,
+                        'bonferroni':False,
+                        'p': d.uns['mncc_p'],
+                        'npos': d.uns['mncc_npos'],
+                        'nneg': d.uns['mncc_nneg'],
+                        'ntotal': len(d)
+                        })
+        results.append({'method': method,
+                        'microniche':False,
+                        'harmonized':False,
+                        'bonferroni':True,
+                        'p': min(d.uns['clustercc_minp'], 1),
+                        'npos': np.nan,
+                        'nneg': np.nan,
+                        'ntotal': len(d)
+                        })
+        
+        for f in glob.glob(f'_results/{dsetname}*_harm.h5ad'):
+            method = os.path.basename(f).split('_')[1]
+            d = sc.read_h5ad(f)
+            results.append({'method': method,
+                            'microniche':False,
+                            'harmonized':True,
+                            'bonferroni':False,
+                            'p': d.uns['clustercc_globalp'],
+                            'npos': d.uns['clustercc_npos'],
+                            'nneg': d.uns['clustercc_nneg'],
+                            'ntotal': len(d)
+                            })
+
+    if D is not None:
+        results.append({'method': 'vima',
+                        'p': D.uns['vima_p'],
+                        'npos': (D.obs.sig_mncoef > 0).sum(),
+                        'nneg': (D.obs.sig_mncoef < 0).sum(),
+                        'ntotal': len(D)
+        })
+
+    results = pd.DataFrame(results).set_index('method')
+    results['frac_pos'] = results.npos / results.ntotal
+    results['frac_neg'] = results.nneg / results.ntotal
+    results.sort_values('p', inplace=True)
+    mainresults = results[
+        ~(results.microniche==True) & ~(results.harmonized==True) & ~(results.bonferroni==True)
+        ][['p','npos','nneg','ntotal','frac_pos','frac_neg']]
+
+    if outpath is not None:
+        supp_outpath = outpath.rsplit('.', 1)
+        supp_outpath = supp_outpath[0] + '_supp.' + supp_outpath[1]
+        results.to_csv(supp_outpath)
+        mainresults.to_csv(outpath)
+
+    return results
+    
