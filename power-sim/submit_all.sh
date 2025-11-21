f14d137df900d5479f18d63af8ecf7a6d925ff41
yakirr <yreshef@broadinstitute.org>
2025-11-21 15:08:25 -0500

    updated power fig

--- power-sim/submit_all.sh
+++ power-sim/submit_all.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 mkdir -p logs
 
-for ratio in 0 0.25 0.5 0.75 1
+# for ratio in 0 0.25 0.5 0.75 1
+for ratio in 0.013 0.38
 do
     echo $ratio
     sbatch --export=RATIO=$ratio,CASEONLY=--caseonly run_sim.sh
 done
 
 # for ratio in 1 1.2 1.4 1.6 1.8 2.0 2.4 2.8
-for ratio in 1.2 1.6
-do
-    echo $ratio
-    sbatch --export=RATIO=$ratio,CASEONLY= run_sim.sh
-done
+# do
+#     echo $ratio
+#     sbatch --export=RATIO=$ratio,CASEONLY= run_sim.sh
+# done
